8989565754a4883c42f44352d827efd8ee671130
Crpearce <Crpearce892@gmail.com>
2023-03-07 15:20:47 -0700

    update nav bar containers, add assets for future components

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,21 +1,24 @@
 import { Outlet } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import {BsFillTelephoneOutboundFill} from "react-icons/bs"
 import "./navigation.styles.css";
 
 const Navigation = ({ tabs }) => {
   return (
-    <div>
-      {/* <aside>
+    <div className="navigation-wrapper">
+      <aside className="phone-container">
         <ul>
           <li>
-            <a href="tel:(720)282-1210" data-icon="A" title="(720)282-1210">
-              <span>(720)282-1210</span>
+            <a href="tel:(720)282-1210" data-icon="A" title="(720)282-1210" className="phone">
+              <BsFillTelephoneOutboundFill  className="phone-icon"/>
+              <span className="phone-number"> (720) 282-1210</span>
             </a>
           </li>
         </ul>
-      </aside> */}
-      <div className="nav-container">
+      </aside>
+      <div className="nav-sections-container">
         <a href="/" aria-label="link to home" className="logo-container">
+          
           <img src={logo} alt="alignment construction logo" className="logo" />
         </a>
         <ul className="nav-menu">
