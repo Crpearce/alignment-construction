2803ffd00e5e0c02c32bcd4ef430ee675b373de2
Crpearce <Crpearce892@gmail.com>
2023-03-09 15:34:30 -0700

    complete projects section

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,6 +2,7 @@ import Concrete from '../../components/concrete/concrete.component';
 import Greeting from '../../components/greeting/greeting.component';
 import Reasons from '../../components/reasons/reasons.component';
 import WorkTypes from '../../components/workTypes/workTypes.component';
+import Projects from '../../components/projects/projects.component';
 import './home.styles.css';
 
 const Home = ({ justification }) => {
@@ -11,6 +12,7 @@ const Home = ({ justification }) => {
             <WorkTypes />
             <Reasons reasons={justification}/>
             <Concrete />
+            <Projects />
         </section>
     );
 }
