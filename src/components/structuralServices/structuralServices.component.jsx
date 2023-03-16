03051b64511c9931a83a43116d88b3ccfc32a804
Crpearce <Crpearce892@gmail.com>
2023-03-16 17:07:24 -0600

    update structural route layout, next step add swiper

--- src/components/structuralServices/structuralServices.component.jsx
+++ src/components/structuralServices/structuralServices.component.jsx
@@ -8,23 +8,23 @@ const StructuralServices = () => {
     AOS.init({ duration: 1250 });
   }, []);
   return (
-    <div className="remodeling-services-wrapper">
-      <div className="remodeling-services-container" data-aos="fade-right">
+    <div className="structural-services-wrapper">
+      <div className="structural-services-container" data-aos="fade-right">
         <h2 className="services-header">SERVICES WE OFFER</h2>
-        <p className="remodeling-details">
+        <p className="structural-details">
           We follow a thorough load-bearing beam installation process to ensure exceptional results. Contact us today for to schedule the following services:
         </p>
-        <ul className="remodeling-list">
+        <ul className="structural-list">
           <li>Inspect your home and provide a free estimate</li>
           <li>Create customer blueprints for your home's design</li>
           <li>Demolish old walls and haul away the waste</li>
           <li>Install quality materials that are built to last</li>
         </ul>
-        <p className="remodeling-details">
+        <p className="structural-details">
           Call ####### today to schedule load-bearing beam installations in Littleton & Aurora, CO.
         </p>
       </div>
-      <div className="remodeling-services-photo" data-aos="fade-left">
+      <div className="structural-services-photo" data-aos="fade-left">
         {/* <img src={basement} alt="remodeled apartment basement" /> */}
       </div>
     </div>
