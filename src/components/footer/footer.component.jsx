f446e05dce4926c9cb727fc5ada428dff8e9bdc5
Crpearce <Crpearce892@gmail.com>
2023-03-16 16:53:25 -0600

    update accessibility score

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <a href="/" aria-label="link to home" className="logo-container">
         <img src={logo} alt="alignment construction logo" className="logo" />
       </a>
-      <h5 className="copyright">Copyright © 2023 Alignment Contruction LLC, all rights reserved.</h5>
+      <span className="copyright">Copyright © 2023 Alignment Contruction LLC, all rights reserved.</span>
       <span className="copyright-address">Littleton, CO 80121 (720) 282-1210</span>
     </div>
   );
