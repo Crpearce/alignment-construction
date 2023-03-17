6ec4850a771cd57b5b40ea65927c4e658890614b
Crpearce <Crpearce892@gmail.com>
2023-03-17 09:54:34 -0600

    add structural photos and hover effects, change flex to column

--- src/components/greeting/greeting.component.jsx
+++ src/components/greeting/greeting.component.jsx
@@ -1,15 +1,20 @@
-import homeBg from "../../assets/homeBg.png";
-import logo from "../../assets/logo.png";
 import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import "./greeting.styles.css";
 import { Link } from "react-router-dom";
 
+import AOS from "aos";
+
+import logo from "../../assets/logo.png";
+import homeBg from "../../assets/homeBg.png";
+
+import "./greeting.styles.css";
+import "aos/dist/aos.css";
+
 const Greeting = () => {
+
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
+  
   return (
     <div
       className="greeting-container"
@@ -25,13 +30,13 @@ const Greeting = () => {
         src={logo}
         alt="alignment construction"
         className="greeting-logo"
-        data-aos="fade-down"
+        data-aos="zoom-in"
       />
       <h3 className="trust">TRUST OUR TEAM TO MODERNIZE YOUR HOME</h3>
       <p className="intro">
         Look to our home remodeling contractor in Littleton & Aurora, CO
       </p>
-      <Link to='/contact'>
+      <Link to="/contact">
         <button>CONTACT US</button>
       </Link>
     </div>
