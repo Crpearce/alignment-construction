b1f1c16f1e7f31e02bea7f63e2c6ffcc7784411f
Crpearce <Crpearce892@gmail.com>
2023-03-22 16:56:31 -0600

    finalize component layout for concrete projects

--- src/routes/concrete/concrete.component.jsx
+++ src/routes/concrete/concrete.component.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import walkway from "../../assets/walkway.png";
+import driveway from "../../assets/driveway.png";
+import patio from "../../assets/patio.png";
+import retaining from "../../assets/retaining.png";
+import foundation from "../../assets/foundation.png";
+
 import AOS from "aos";
 import "./concrete.styles.css";
 import "aos/dist/aos.css";
@@ -78,42 +83,65 @@ const Concrete = () => {
       </div>
       <div className="concrete-project-types-container">
         <div className="concrete-type-driveway">
-          <h2 className="concrete-additions-header1" data-aos="fade-in">
+          <h2 className="concrete-additions-header1" data-aos="flip-up">
             DRIVEWAYS
           </h2>
+          <h6 className="pitch">
+            RELY ON US FOR A TOP-OF-THE-LINE DRIVEWAY INSTALLATION
+          </h6>
           <div className="concrete-additions-info-container">
-            <p className="concrete-additions-info">
+            <p className="concrete-additions-info1" data-aos="fade-right">
               Driveways aren't just for parking - they also affect your home's
-              curb appeal. That's why you should hire Alignment Construction LLC
-              for concrete driveway installations. Our team of experts will
-              install an elegant driveway that will last for years to come.
+              curb appeal. Does your driveway have cracks, potholes, drainage
+              problems or an overall rough look? A new driveway can increase
+              your property's value between 5 and 10 percent. That's why you
+              should hire Alignment Construction LLC for concrete driveway
+              installations. Our team of experts will install an elegant
+              driveway that will last for years to come.
             </p>
-            <img src={walkway} className='concrete-additions-photo'/>
+            <img
+              src={driveway}
+              className="concrete-additions-photo"
+              alt="concrete project"
+            />
           </div>
         </div>
         <div className="concrete-type-patio">
-          <h2 className="concrete-additions-header2" data-aos="fade-in">
+          <h2 className="concrete-additions-header2" data-aos="flip-up">
             PATIOS
           </h2>
+          <h5 className="pitch">
+            WE OFFER PLENTY OF DECORATIVE CONCRETE PATIO OPTIONS TO GIVE YOU THE
+            EXACT LOOK YOU'RE GOING FOR
+          </h5>
           <div className="concrete-additions-info-container">
-            <img src={walkway} className='concrete-additions-photo'/>
-            <p className="concrete-additions-info">
+            <img
+              src={patio}
+              className="concrete-additions-photo"
+              alt="concrete project"
+            />
+            <p className="concrete-additions-info2" data-aos="fade-left">
               Do you want the perfect spot to sit and enjoy the weather? A
               decorative concrete patio can make your property more attractive
               while adding a new space to your home. Alignment Construction in
-              Littleton & Aurora, CO and surrounding areas will complete your
-              concrete patio installation correctly. We'll add a stain-resistant
-              sealing to make this the perfect choice for pool decks or barbecue
-              spaces.
+              will complete your concrete patio installation correctly. We'll
+              add a stain-resistant sealing to make this the perfect choice for
+              pool decks or barbecue spaces. A concrete patio installation will
+              benefit your home by increasing your property value, boosting your
+              curb appeal, and giving you a chance to customize your yard.
             </p>
           </div>
         </div>
         <div className="concrete-type-retaining">
-          <h2 className="concrete-additions-header1" data-aos="fade-in">
+          <h2 className="concrete-additions-header1" data-aos="flip-up">
             RETAINING WALLS
           </h2>
+          <h6 className="pitch">
+            A NEW RETAINING WALL WILL HELP PREVENT EROSION AND MAKE YOUR YARD
+            LOOK BEAUTIFUL
+          </h6>
           <div className="concrete-additions-info-container">
-            <p className="concrete-additions-info">
+            <p className="concrete-additions-info1" data-aos="fade-right">
               If your property is on a slope, it will erode more easily. Protect
               your landscape by getting a retaining wall from Alignment
               Construction. We offer retaining wall installation services for
@@ -122,23 +150,33 @@ const Concrete = () => {
               contractor will use top-of-the-line tools and materials to create
               your retaining wall.
             </p>
-            <img src={walkway} className='concrete-additions-photo'/>
+            <img
+              src={retaining}
+              className="concrete-additions-photo"
+              alt="concrete project"
+            />
           </div>
         </div>
         <div className="concrete-type-foundation">
-          <h2 className="concrete-additions-header2" data-aos="fade-in">
+          <h2 className="concrete-additions-header2" data-aos="flip-up">
             FOUNDATIONS
           </h2>
+          <h5 className="pitch">
+            A STRONG BUILD STARTS WITH THE RIGHT FOUNDATION
+          </h5>
           <div className="concrete-additions-info-container">
-            <img src={walkway}
-            className='concrete-additions-photo'/>
-            <p className="concrete-additions-info">
-              When the time comes to pour the foundation at your new
-              construction project, contact Alignment Construction in Littleton,
-              CO and surrounding areas. With over 15 years experience of
-              concrete foundation experience in Littleton & Aurora, CO, we
-              should be your top choice for the job. You can trust us to
-              construct a foundation that's built to last.
+            <img
+              src={foundation}
+              className="concrete-additions-photo"
+              alt="concrete project"
+            />
+            <p className="concrete-additions-info2" data-aos="fade-left">
+              Your new home or commercial build is a huge investment. When the
+              time comes to pour the foundation at your new construction
+              project, contact Alignment Construction. With over 15 years
+              experience of concrete foundation experience, we should be your
+              top choice for the job. You can trust us to form, pour, and set a
+              durable foundation that's built to last.
             </p>
           </div>
         </div>
