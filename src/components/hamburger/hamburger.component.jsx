02ee33d64966aa79b467f72c2aa6c87124a7a30c
Crpearce <Crpearce892@gmail.com>
2023-03-28 15:52:24 -0600

    update hamburger nav with state, modal will appear on hamburger click

--- src/components/hamburger/hamburger.component.jsx
+++ src/components/hamburger/hamburger.component.jsx
@@ -1,11 +1,22 @@
 import "./hamburger.styles.css";
-
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+import Specialization from "../specialization/specialization.component";
 
 const Hamburger = () => {
+  const [hamburgerMenu, setHamburgerMenu] = useState(null);
+
+  const showNavHandler = () => {
+    setHamburgerMenu(true);
+  };
+
   return (
     <section className="hamburger-container">
-      <GiHamburgerMenu className="hamburger-icon" />
+      {hamburgerMenu === true ? (
+        <Specialization />
+      ) : (
+        <GiHamburgerMenu className="hamburger-icon" onClick={showNavHandler} />
+      )}
     </section>
   );
 };
